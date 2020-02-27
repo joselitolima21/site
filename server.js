c40b168298babda133853b6e9f4b0b9a7c2eaa86
Joselito Lima <joselitolima90@hotmail.com>
2020-02-27 16:44:09 -0300

    Algumas mudanças

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const server = express()
 //Arquivos estáticos
 server.use(express.static('public'))
 
+const baseDir = `${__dirname}/public/`
+
 //Renderizar html
-nunjucks.configure("./public/",{
+nunjucks.configure(`${baseDir}`,{
     express: server
 })
 
 server.get('/',(req,res)=>{
     return res.render("index.html")
 })
-server.listen(3000)
+
+const port = process.env.PORT || 3000
+
+server.listen(port,() => console.log(`Servidor subiu com sucesso em http://localhost:${port}`))
